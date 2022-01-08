7cf6d9ee4c9b02eae27a5eb0ffd80bd478b0d47d
leandro <leandroyamada@msn.com>
2022-01-08 01:39:12 -0300

    resolvido exercicio 1 (Classe Bola)

---
+++ bola.py
@@ -0,0 +1,17 @@
+class Bola():
+    def __init__(self, cor, circunferencia, material):
+        self.cor = cor
+        self.circunferencia = circunferencia
+        self.material = material
+
+    def trocaCor(self, cor):
+        self.cor = cor
+
+    def mostraCor(self):
+        return self.cor
+
+b = Bola("azul", 10, 'couro')
+print(b.mostraCor())
+b.trocaCor('vermelha')
+print(b.mostraCor())
+
