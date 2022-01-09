6acc302bc4e1be8e55eaa61f8a1cc4f6a4049018
leandro <leandroyamada@msn.com>
2022-01-08 23:51:31 -0300

    resolvido exercicio 2 (Classe Quadrado)

---
+++ quadrado.py
@@ -0,0 +1,19 @@
+class Quadrado():
+    def __init__(self, lado):
+        self.setLado(lado)
+
+    def setLado(self, lado):
+        self.lado = lado
+
+    def getLado(self):
+        return self.lado
+
+    def calcularArea(self, ):
+        return self.lado * self.lado
+
+
+
+q = Quadrado(5)
+print(q.getLado())
+q.setLado(10)
+print(q.calcularArea())
