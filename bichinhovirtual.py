cfe39c4bd857839164ed21cc883dfd99a08b69e9
leandro <leandroyamada@msn.com>
2022-01-22 01:48:08 -0300

    resolvido exercicio 7 (Classe Bichinho Virtual)

---
+++ bichinhovirtual.py
@@ -0,0 +1,37 @@
+class BichinhoVirtual:
+    def __init__(self, nome, fome, saude, idade):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
+
+    def alterar_nome(self, nome):
+        self.nome = nome
+
+    def alterar_fome(self, fome):
+        self.fome = fome
+
+    def alterar_saude(self, saude):
+        self.saude = saude
+
+    def alterar_idade(self, idade):
+        self.idade = idade
+
+    def get_nome(self):
+        return self.nome
+
+    def get_fome(self):
+        return self.fome
+
+    def get_saude(self):
+        return self.saude
+
+    def get_idade(self):
+        return self.idade
+
+    def humor(self):
+        return self.get_fome() * self.get_saude()
+
+
+b = BichinhoVirtual('Tamagoshi', 8, 5, 12)
+print(b.humor())
