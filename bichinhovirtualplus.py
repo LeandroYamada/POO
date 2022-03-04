caf53c23cc80ceb67d93704ee521ca5ba8d61582
leandro <divine321voice>
2022-03-04 19:19:31 -0300

    resolvido exercicio 15 (Classe Bichinho Virtual ++)

---
+++ bichinhovirtualplus.py
@@ -0,0 +1,49 @@
+class BichinhoVirtual:
+    def __init__(self, nome, fome, saude, idade):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
+
+    def alterar_nome(self, nome):
+        self.nome = nome
+
+    def alterar_fome(self, fome):
+        self.fome = fome
+
+    def alterar_saude(self, saude):
+        self.saude = saude
+
+    def alterar_idade(self, idade):
+        self.idade = idade
+
+    def get_nome(self):
+        return self.nome
+
+    def get_fome(self):
+        return self.fome
+
+    def get_saude(self):
+        return self.saude
+
+    def get_idade(self):
+        return self.idade
+
+    def humor(self):
+        return self.get_fome() * self.get_saude()
+
+    def alimentar(self, qtd):
+        if (qtd >= 0) and (qtd <= 100):
+            self.fome -= self.fome * (qtd / 100.0)
+
+    def brincar(self, qtd):
+        if (qtd >= 0) and (qtd <= 100):
+            self.saude += self.saude * (qtd / 100.0)
+
+
+b = BichinhoVirtual('Tamagoshi', 8, 5, 12)
+print(b.humor())
+b.alimentar(50)
+print(b.humor())
+b.brincar(30)
+print(b.humor())
